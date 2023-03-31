fb33088e3ed9c71c2385c30db8bcd5a1ee6f7edd
Jakub Jaskow <jakub.jaskow@nokia.com>
2023-03-31 15:16:47 +0200

    Add insertion sort

--- IS.cpp
+++ IS.cpp
@@ -10,13 +10,35 @@ int main(int argc, char* argv[])
     std::cin >> line;
     size_t n = std::stol(line);
     size_t arr[n];
-    for(size_t i = 0; i < n; i++)
+    for (size_t &i : arr)
     {
         std::cin >> line;
-        arr[i] = stol(line);
+        i = stol(line);
+    }
+    std::cout << std::endl << "RNG array:" << std::endl;
+    for (size_t i : arr)
+    {
+        std::cout << i << std::endl;
     }
 
-    
+    for(size_t i = 1 ; i < n ;i++)
+    {
+        size_t j = i - 1;
+        size_t x = arr[i];
+        while(arr[j] > x && j > 0)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = x;
+    }
+
+    std::cout << std::endl << "Sorted array:" << std::endl;
+
+    for(size_t &i : arr)
+    {
+        std::cout << i << std::endl;
+    }
 
 
     return 0;
