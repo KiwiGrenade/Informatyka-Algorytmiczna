f5fc765675d510589039eb9b2992dc4e6c61d1e6
Mango T14 <kubajaskow@wp.pl>
2023-03-31 12:46:07 +0200

    Change IS

--- IS.cpp
+++ IS.cpp
@@ -3,10 +3,21 @@
 //
 #include <iostream>
 #include <random>
+
 int main(int argc, char* argv[])
 {
-    std::string elo;
-    std::cin >> elo;
-    std::cout << elo;
+    std::string line;
+    std::cin >> line;
+    size_t n = std::stol(line);
+    size_t arr[n];
+    for(size_t i = 0; i < n; i++)
+    {
+        std::cin >> line;
+        arr[i] = stol(line);
+    }
+
+    
+
+
     return 0;
 }
--- RNG.cpp
+++ RNG.cpp
@@ -6,6 +6,9 @@ int main(int argc, char* argv[])
     size_t seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::mt19937 generator (seed);
     size_t n = std::stol(argv[1]);
+
+    std::cout << n << std::endl;
+
     for (int i = 0; i < n; ++i)
     {
         std::cout << generator() << std::endl;
