cd899bd3c456247ec977887010e39ba9d02998a9
Mango T14 <kubajaskow@wp.pl>
2023-04-02 11:24:06 +0200

    Finished ex. 4

--- HYB.cpp
+++ HYB.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <random>
 #include <list>
-
-void printArr(size_t arr[], size_t n)
+void printArr(size_t arr[], size_t n) noexcept
 {
     for (size_t i = 0; i < n; i++)
     {
@@ -10,7 +9,8 @@ void printArr(size_t arr[], size_t n)
     }
 }
 
-void printArrState(size_t arr[], size_t n) noexcept {
+void printArrState(size_t arr[], size_t n) noexcept
+{
     for (size_t i = 0; i < n; i++)
     {
         std::cout << arr[i] << " ";
@@ -27,9 +27,9 @@ bool comp(size_t arr_j, size_t x)
     return arr_j > x;
 }
 
-void insertionSort(size_t arr[], long p, long r, size_t n)
+void insertionSort(size_t arr[], long p ,long r, long n)
 {
-    for(size_t i = 1 ; i < n ;i++)
+    for(long i = p; i < r ;i++)
     {
         //j can be negative
         long j = i - 1;
@@ -52,25 +52,67 @@ void insertionSort(size_t arr[], long p, long r, size_t n)
     }
 }
 
-int main()
+long partition(size_t arr[], long p, long r, long n) noexcept
+{
+    //i can be negatice
+    long pivot = arr[r];
+    long i = p - 1;
+    for(long long j = p; j < r; j++)
+    {
+        nComp++;
+        if(arr[j] <= pivot)
+        {
+            i++;
+            nSwap++;
+            std::swap(arr[i], arr[j]);
+            if(n < 40)
+            {
+                printArrState(arr, n);
+            }
+        }
+    }
+    nSwap++;
+    std::swap(arr[i+1], arr[r]);
+    if(n < 40)
+    {
+        printArrState(arr, n);
+    }
+    return i+1;
+}
+
+void quickSort(size_t arr[], long p, long r, long n, long limit) noexcept
+{
+    if(p < r)
+    {
+        long long q = partition(arr, p, r, n);
+        if(q - p < 5)
+        {
+            insertionSort(arr, p, q - 1, n);
+            insertionSort(arr, q + 1, r, n);
+        }
+        quickSort(arr, p, q - 1, n, limit);
+        quickSort(arr, q + 1, r, n, limit);
+    }
+}
+
+int main(int argc, char* argv[])
 {
     std::string line;
     std::cin >> line;
     size_t n = std::stol(line);
     size_t arr[n];
-    for (size_t &i : arr)
+    for (size_t i = 0; i < n; i++)
     {
         std::cin >> line;
-        i = stol(line);
+        arr[i] = stol(line);
     }
-
     if(n < 40)
     {
         std::cout << "Random numbers:" << std::endl;
         printArr(arr, n);
     }
 
-    insertionSort(arr, 0, n-1, n);
+    quickSort(arr, 0, n - 1, n, std::stol(argv[1]));
 
     if(n < 40)
     {
