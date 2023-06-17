2d75e472437e02b3d76049acd44ae70371994fea
Mango T14 <kubajaskow@wp.pl>
2023-06-17 18:48:55 +0200

    Style check whole project

--- RNG.cpp
+++ RNG.cpp
@@ -2,26 +2,26 @@
 #include <chrono>
 #include <random>
 #include <unordered_set>
-int main(int argc, char* argv[])
-{
+
+int main(int argc, char *argv[]) {
     size_t seed = std::chrono::system_clock::now().time_since_epoch().count();
     size_t n = std::stol(argv[1]);
-    std::mt19937 generator (seed);
-    std::uniform_int_distribution<> distribution (65, 90);
+    std::mt19937 generator(seed);
+    std::uniform_int_distribution<> distribution(1, 2 * n - 1);
 
-    std::vector<char> set;
-    std::vector<char> set1;
+    std::unordered_set<std::size_t> set;
+    std::unordered_set<std::size_t> set1;
 
-    while(set.size() < n)
-        set.push_back((char) distribution(generator));
-    while(set1.size() < n)
-        set1.push_back((char) distribution(generator));
+    while (set.size() < n)
+        set.emplace(distribution(generator));
+    while (set1.size() < n)
+        set1.emplace(distribution(generator));
 
     std::cout << n << std::endl;
 
-    for (auto i : set)
+    for (auto i: set)
         std::cout << i << std::endl;
-    for (auto i : set1)
+    for (auto i: set1)
         std::cout << i << std::endl;
     return 0;
 }
