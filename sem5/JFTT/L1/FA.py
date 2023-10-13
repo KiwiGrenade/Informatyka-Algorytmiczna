0d043f86933febdd18754d6ad5022de4452fe136
Mango T14 <kubajaskow@wp.pl>
2023-10-13 18:40:32 +0200

    BUGFIX
    PROBLEM: FA returns wrong results when |P| > 1.
    CAUSE: Wrong array initialization. Python has "shallow arrays" which causes change in row X to be reflected in row Y.
    SOLUTION: Change array initialization method to correct/current one.

---
+++ sem5/JFTT/L1/FA.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def comp_trans_func(P, m):
+    utf8_len = 1112064 - 1
+    # fuck python and those fucking shitty 2d lists. Who the fuck uses ShAloOw LiSt$
+    trans_func = [[0 for i in range(utf8_len)] for j in range(m + 1)]  # counting 0 we have m+1 states
+    for q in range(m):
+        for a in range(utf8_len):
+            k = min(m, q + 1)
+            while not (str(P[:q] + chr(a)).endswith(P[:k])):
+                k = k - 1
+            trans_func[q][a] = k
+    return trans_func
+
+
+def FA():
+    args = sys.argv[1:]  # cut off script name
+    P = args[0]
+    m = len(P)
+
+    transition_function = comp_trans_func(P, m)
+
+    # get text
+    file_name = args[1]
+    file = open(file_name)
+    T = file.read()
+    n = len(T)
+
+    q = 0
+
+    for i in range(n):
+        q = transition_function[q][ord(T[i])]
+        if q == m:
+            print(f'Pattern occurs with shift: {i + 1 - m}')
+
+    return 0
+
+
+if __name__ == '__main__':
+    FA()
