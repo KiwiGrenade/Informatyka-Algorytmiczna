da82836e108ba2ad6d53a7ac97d7ceedcd21a3cb
Jakub Jaskow <jakub.jaskow@nokia.com>
2023-10-21 14:37:30 +0200

    Refactor

--- sem5/ON/L1/ex2.jl
+++ sem5/ON/L1/ex2.jl
@@ -1,8 +1,7 @@
 # Jakub Jaśków
+floatTypes = [Float16, Float32, Float64]
 println("Zadanie 2")
-println("\nFloat16(1 - 3(4/3-1)) = ", Float16(1.0 - 3.0*Float16(Float16(4.0 / 3.0) - 1.0)))
-println("macheps(Float16) = ", eps(Float16))
-println("\nFloat32(1 - 3(4/3-1)) = ", Float32(1.0 - 3.0*Float32(Float32(4.0 / 3.0) - 1.0)))
-println("macheps(Float32) = ", eps(Float32))
-println("\nFloat64(1 - 3(4/3-1)) = ", Float64(1.0 - 3.0*Float64(Float64(4.0 / 3.0) - 1.0)))
-println("macheps(Float64) = ", eps(Float64))
+for type in floatTypes
+    println('\n', type, "(3(4/3-1) - 1) = ", type(3.0*type(type(4.0 / 3.0) - 1.0) - 1))
+    println("macheps(", type, ") = ", eps(type))
+end
