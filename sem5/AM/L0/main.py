e18203fc501f243eae0b255faf66fbce2f38172c
Jakub Jaskow <jakub.jaskow@nokia.com>
2023-10-23 14:38:20 +0200

    Done. Fuck you and see you tommorow

--- sem5/AM/L0/main.py
+++ sem5/AM/L0/main.py
@@ -6,65 +6,83 @@ import tsplib95 as tsp
 from numpy.random import shuffle
 import math
 
-
 def get_path_cost(G, path):
     distance = 0
     for i in range(len(path) - 1):
-        u = G.nodes[path[i]]
-        v = G.nodes[path[i + 1]]
+        distance += G.edges[path[i], path[i+1]]["weight"]
+    # add cost of return path
+    distance += G.edges[path[0], path[-1]]["weight"]
 
-        coords = [u["coord"], v["coord"]]
-        distance += math.dist(coords[0], coords[1])  # sum(dist(first_of, last of))
-
-    distance += math.dist(G.nodes[path[-1]]["coord"], G.nodes[path[0]]["coord"])  # dist(last_of, first_of)
     return distance
 
-
-def plot_data(G, path, file_name):
+def get_minimums(G, path):
+    print("Getting minimums...")
     path = path[:-1]  # cut off last element
 
-    min_10_list = min_50_list = []
-    min_10 = min_50 = min_1000 = math.inf
+    min_10_list_y = []
+    min_10_list_x = []
+    min_50_list_y = []
+    min_50_list_x = []
+    min_10_x = 0
+    min_10_y = math.inf
+    min_50_x = 0
+    min_50_y = math.inf
+    min_1000_x = 0
+    min_1000_y = math.inf
     for i in range(1, 1001):
         # permutate
         shuffle(path)
 
         cost = get_path_cost(G, path)
-
-        min_1000 = min(min_1000, cost)
-        min_50 = min(min_50, cost)
-        min_10 = min(min_10, cost)
+        if cost < min_10_y:
+            min_10_x = i
+            min_10_y = cost
+        if cost < min_50_y:
+            min_50_x = i
+            min_50_y = cost
+        if cost < min_1000_y:
+            min_1000_x = i
+            min_1000_y = cost
         if i % 10 == 0:
-            min_10_list.append(min_10)
-            min_10 = math.inf
+            min_10_list_y.append(min_10_y)
+            min_10_list_x.append(min_10_x)
+            min_10_y = math.inf
         if i % 50 == 0:
-            min_50_list.append(min_50)
-            min_50 = math.inf
-    print(min_1000)
+            min_50_list_y.append(min_50_y)
+            min_50_list_x.append(min_50_x)
+            min_50_y = math.inf
+    min_10_list = [min_10_list_x, min_10_list_y]
+    min_50_list = [min_50_list_x, min_50_list_y]
+    min_1000 = [min_1000_x, min_1000_y]
+    return min_10_list, min_50_list, min_1000
+
+
+def plot_data(min_10_list, min_50_list, min_1000, file_name):
+    print(f'Plotting min_{file_name}...')
+
     # Create a dot plot for the red data
-    plt.plot(min_1000, 'ro', markersize=12, label='Red Dots')
+    plt.scatter(min_1000[0], min_1000[1], c="red", s=150, label='1000 Minimum')
 
     # Add blue dots to the same plot
-    plt.plot(min_50_list, 'bo', markersize=8, label='Blue Dots')
+    plt.scatter(min_50_list[0], min_50_list[1],c="orange", s= 40, label='50 Minimum')
 
     # Add yellow dots to the same plot
-    plt.plot(min_10_list, 'yo', markersize=5, label='Yellow Dots')
+    plt.scatter(min_10_list[0], min_10_list[1], c="blue", s = 8,label='10 Minimum')
 
     # Add labels and a title
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Dot Plot')
+    plt.title(file_name)
 
     # Add a legend
     plt.legend()
 
     # Display the plot
     plt.grid(True)  # Add a grid
-    plt.savefig(f"graphs/{file_name}")
+    plt.savefig(f"graphs/min_{file_name}")
     plt.show()
 
 
 def plot_path(G, path, file_name):
+    print(f'Plotting path_{file_name}...')
     path_graph = nx.path_graph(path, G)
     nx.draw(path_graph, node_size=7)
     plt.savefig(f'graphs/path_{file_name}')
@@ -72,22 +90,26 @@ def plot_path(G, path, file_name):
 
 
 def plot_MST(G, file_name):
+    print(f'Plotting MST{file_name}...')
     # create MST, draw and save
     MST = nx.minimum_spanning_tree(G)
     nx.draw(MST, node_size=5)
-    plt.savefig(f'graphs/MST_{file_name}.pdf')
+    plt.savefig(f'graphs/MST_{file_name}')
     plt.show()
-
 
 for file_name in os.listdir('inputs'):
     # load problem from .tsp file
+    # file_name = "xqf131.tsp"
     problem = tsp.load(f'inputs/{file_name}')
     # convert the problem to a networkx graph
     G = problem.get_graph()
 
     file_name = file_name[:-4]
 
+    print("Getting path...")
     path = nx.algorithms.approximation.traveling_salesman_problem(G)
+    path = path[:-1]
     # plot_MST(G, file_name)
-    plot_path(G, path, file_name)
-    # plot_data(G, path, file_name)
+    # plot_path(G, path, file_name)
+    min_10_list, min_50_list, min_1000 = get_minimums(G, path)
+    plot_data(min_10_list, min_50_list, min_1000, file_name)
