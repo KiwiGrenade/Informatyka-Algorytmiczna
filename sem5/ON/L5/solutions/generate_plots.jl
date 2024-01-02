99dd2613aa17ffff3b31c68d38376d2bac2fc122
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2024-01-02 15:41:54 +0100

    Refactor blocksys.jl and generate_plots.jl

--- sem5/ON/L5/solutions/generate_plots.jl
+++ sem5/ON/L5/solutions/generate_plots.jl
@@ -11,56 +11,52 @@ sub_size = 4
 cond = Float64(1.0)
 
 # Tests results
-times_gauss = zeros(length(mat_sizes))
-times_gauss_choose = zeros(length(mat_sizes))
+time = zeros(length(mat_sizes))
+time_choose = zeros(length(mat_sizes))
 
-memory_gauss = zeros(length(mat_sizes))
-memory_gauss_choose = zeros(length(mat_sizes))
+memory = zeros(length(mat_sizes))
+memory_choose = zeros(length(mat_sizes))
 
-operations_gauss = zeros(length(mat_sizes))
-operations_gauss_choose = zeros(length(mat_sizes))
+oper_num = zeros(length(mat_sizes))
+oper_num_choose = zeros(length(mat_sizes))
 
 
 for i = 1:length(mat_sizes)
-
-    # Generating matrix for tests
     blockmat(mat_sizes[i], sub_size, cond, "test_matrix.txt")
     println("Matrix size: ", mat_sizes[i])
     println("Submatrices size: ", sub_size)
 
-    # Loading generated matrix and computing vector of right sides
     matrix = load_matrix("test_matrix.txt")
     vec = generate_right_side_vector(matrix[1], matrix[2], matrix[3])
 
     #### Gauss
     results = @timed solve_gauss(matrix[1], vec, matrix[2], matrix[3])
     
-    times_gauss[i] = results[2] 
-    memory_gauss[i] = results[3]
-    operations_gauss[i] = mat_sizes[i] * sub_size^2 + 2 * mat_sizes[i]
+    time[i] = results[2] 
+    memory[i] = results[3]
+    oper_num[i] = mat_sizes[i] * sub_size^2 + 2 * mat_sizes[i]
 
-    # Loading generated matrix and computing vector of right sides
     matrix = load_matrix("test_matrix.txt")
     vec = generate_right_side_vector(matrix[1], matrix[2], matrix[3])
 
     #### Gauss with choose
     results = @timed solve_gauss_with_choose(matrix[1], vec, matrix[2], matrix[3])
     
-    times_gauss_choose[i] = results[2]
-    memory_gauss_choose[i] = results[3]
-    operations_gauss_choose[i] = mat_sizes[i] * sub_size^3 + 2 * mat_sizes[i]
+    time_choose[i] = results[2]
+    memory_choose[i] = results[3]
+    oper_num_choose[i] = mat_sizes[i] * sub_size^3 + 2 * mat_sizes[i]
 end
 
 #### Solving
 ### Plots
 # Time
-plot(mat_sizes, [times_gauss times_gauss_choose], label=["Gauss" "Gauss with choose"], linewidth=[3 3])
-png("plots/times_solving")
+plot(mat_sizes, [time time_choose], label=["Gauss" "Gauss with choose"], linewidth=[3 3])
+png("plots/time_solving")
 # Memory
-plot(mat_sizes, [memory_gauss memory_gauss_choose], label=["Gauss" "Gauss with choose"], linewidth=[3 3])
+plot(mat_sizes, [memory memory_choose], label=["Gauss" "Gauss with choose"], linewidth=[3 3])
 png("plots/memory_solving")
 # Operations
-plot(mat_sizes, [operations_gauss operations_gauss_choose], label=["Gauss" "Gauss with choose"], linewidth=[3 3])
+plot(mat_sizes, [oper_num oper_num_choose], label=["Gauss" "Gauss with choose"], linewidth=[3 3])
 png("plots/operations_solving")
 
 rm("test_matrix.txt")
