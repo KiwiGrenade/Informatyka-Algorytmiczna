2b84b5b24e9f420a0d9295f9c1493f588a89e88c
Mango T14 <kubajaskow@wp.pl>
2024-03-15 00:13:20 +0100

    Change E(R/I)GCD implementations.

--- electives/JPP/L1/libs/lib.h
+++ electives/JPP/L1/libs/lib.h
@@ -11,15 +11,23 @@ uint64_t RFactor(uint16_t n);
 uint64_t IGCD(uint64_t a, uint64_t b);
 uint64_t RGCD(uint64_t a, uint64_t b);
 
-struct result IEEA(int64_t a, int64_t b);
-struct result REEA(int64_t a, int64_t b);
+//struct result IEEA(int64_t a, int64_t b);
+//struct result REEA(int64_t a, int64_t b);
+struct int64_pair ILDE(uint64_t a, uint64_t b, uint64_t c);
+struct int64_pair RLDE(uint64_t a, uint64_t b, uint64_t c);
 
-struct result
+struct int64_pair
 {
-    int64_t d;
     int64_t x;
     int64_t y;
 };
+
+//struct result
+//{
+//    int64_t d;
+//    int64_t x;
+//    int64_t y;
+//};
 
 #ifdef __cplusplus
 }
