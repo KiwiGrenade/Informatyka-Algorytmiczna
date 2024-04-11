85a503dffc2b3e503e6ee14d1355d0b6b3c3fff7
Jakub Jaskow <jakub.jaskow@nokia.com>
2024-04-11 21:10:14 +0200

    Add ex02, 03, 04 and 06.

---
+++ electives/WZA/ex03/genplot.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib import cm
+
+def first_variety():
+    plt.style.use('_mpl-gallery')
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = X**2 + Y**2
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Blues)
+
+    plt.title(r'V($z -x^2 - y^2$)', y=1)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
+
+def second_variety():
+    plt.style.use('_mpl-gallery')
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = np.sqrt(X**2 + Y**2)
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Reds)
+
+    plt.title(r'V($z^2 -x^2 - y^2$)', y=1)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
+
+def third_variety():
+    plt.style.use('_mpl-gallery')
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = X**2 - Y**2
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Greens)
+    plt.title(r'V($z -x^2 + y^2$)', y=1)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
+
+def fourth_variety():
+    plt.style.use('_mpl-gallery')
+    X = np.arange(-5, 5, 0.25)
+    Y = np.zeros(int(10/0.25))
+    Z = 0
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    plt.plot(X, Y, color='green')
+
+    plt.title(r'V($xz, yz$)', y=1)
+    Z = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    Z, Y = np.meshgrid(Z, Y)
+    X = 0
+
+    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Greens)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
+
+first_variety()
+second_variety()
+third_variety()
+fourth_variety()
