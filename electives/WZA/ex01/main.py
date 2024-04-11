509fcc06d9f8324e6889cf6b305269a661a53b37
Jakub Jaskow <jakub.jaskow@nokia.com>
2024-04-11 21:23:52 +0200

    Fix bloopers and update README.md

--- electives/WZA/ex01/main.py
+++ electives/WZA/ex01/main.py
@@ -3,8 +3,8 @@ import sys
 from lib import *
 
 def main() -> int:
-    gc1 = complex_gauss(3, 4)
-    gc2 = complex_gauss(1, 3)
+    gc1 = ComplexGauss(3, 4)
+    gc2 = ComplexGauss(1, 3)
 
     c = gcd(gc1, gc2)
     d = lcm(gc1, gc2)
--- electives/WZA/ex03/genplot.py
+++ electives/WZA/ex03/genplot.py
@@ -3,15 +3,16 @@ import numpy as np
 
 from matplotlib import cm
 
+
 def first_variety():
     plt.style.use('_mpl-gallery')
     X = np.arange(-5, 5, 0.25)
     Y = np.arange(-5, 5, 0.25)
     X, Y = np.meshgrid(X, Y)
-    Z = X**2 + Y**2
+    Z = X ** 2 + Y ** 2
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Blues)
+    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Oranges)
 
     plt.title(r'V($z -x^2 - y^2$)', y=1)
 
@@ -21,12 +22,13 @@ def first_variety():
 
     plt.show()
 
+
 def second_variety():
     plt.style.use('_mpl-gallery')
     X = np.arange(-5, 5, 0.25)
     Y = np.arange(-5, 5, 0.25)
     X, Y = np.meshgrid(X, Y)
-    Z = np.sqrt(X**2 + Y**2)
+    Z = np.sqrt(X ** 2 + Y ** 2)
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Reds)
@@ -39,12 +41,13 @@ def second_variety():
 
     plt.show()
 
+
 def third_variety():
     plt.style.use('_mpl-gallery')
     X = np.arange(-5, 5, 0.25)
     Y = np.arange(-5, 5, 0.25)
     X, Y = np.meshgrid(X, Y)
-    Z = X**2 - Y**2
+    Z = X ** 2 - Y ** 2
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Greens)
@@ -55,10 +58,11 @@ def third_variety():
 
     plt.show()
 
+
 def fourth_variety():
     plt.style.use('_mpl-gallery')
     X = np.arange(-5, 5, 0.25)
-    Y = np.zeros(int(10/0.25))
+    Y = np.zeros(int(10 / 0.25))
     Z = 0
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     plt.plot(X, Y, color='green')
@@ -69,13 +73,14 @@ def fourth_variety():
     Z, Y = np.meshgrid(Z, Y)
     X = 0
 
-    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Greens)
+    ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Blues)
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
     plt.show()
+
 
 first_variety()
 second_variety()
