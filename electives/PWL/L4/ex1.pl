adf2f418c73f4d2634351843705dae48f59f9a21
Jakub Jaskow <jakub.jaskow@nokia.com>
2024-04-18 07:35:11 +0200

    Add better description

--- electives/PWL/L4/ex1.pl
+++ electives/PWL/L4/ex1.pl
@@ -1,22 +1,23 @@
 base_wyrazenie([X], X, X).
 base_wyrazenie(List, Result, X+Y) :-
-    apply_operator(List, X, Y, Temp1, Temp2),
-    Result is Temp1+Temp2.
+    apply_operator(List, X, Y, Value1, Value2),
+    Result is Value1+Value2.
 base_wyrazenie(List, Result, X-Y) :-
-    apply_operator(List, X, Y, Temp1, Temp2),
-    Result is Temp1-Temp2.
+    apply_operator(List, X, Y, Value1, Value2),
+    Result is Value1-Value2.
 base_wyrazenie(List, Result, X*Y) :-
-    apply_operator(List, X, Y, Temp1, Temp2),
-    Result is Temp1*Temp2.
+    apply_operator(List, X, Y, Value1, Value2),
+    Result is Value1*Value2.
 base_wyrazenie(List, Result, X/Y) :-
-    apply_operator(List, X, Y, Temp1, Temp2),
-    Temp2=\=0,
-    Result is Temp1/Temp2.
+    apply_operator(List, X, Y, Value1, Value2),
+    Value2=\=0,
+    Result is Value1/Value2.
 
-apply_operator(List, Expr1, Expr2, Temp1, Temp2) :-
+apply_operator(List, Expr1, Expr2, Value1, Value2) :-
     append([List1|List2], [List3|List4], List),
-    base_wyrazenie([List1|List2], Temp1, Expr1),
-    base_wyrazenie([List3|List4], Temp2, Expr2).
+    base_wyrazenie([List1|List2], Value1, Expr1),
+    base_wyrazenie([List3|List4], Value2, Expr2).
 
 wyrazenie(L, V, E) :-
-    base_wyrazenie(L, V, E), !.
+    base_wyrazenie(L, V, E),
+    !.
