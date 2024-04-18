525ee3781a03bc22d75fe175b89d48957e02c0f7
Jakub Jaskow <jakub.jaskow@nokia.com>
2024-04-18 11:08:57 +0200

    Fix pin board configuration

--- sem6/Embedded/L4/sketch_mar14a/sketch_mar14a.ino
+++ sem6/Embedded/L4/sketch_mar14a/sketch_mar14a.ino
@@ -30,7 +30,7 @@ uint8_t arrowRight[8] =
 int argIn = 0;
 
 void WHL_setup() {
-  w.attach(7, 8, 5, 12, 11, 10);
+  w.attach(7, 8, 5, 12, 11, 6);
   
   Serial.println("Forward: WAD");
   Serial.println("Back: ZXC");
@@ -58,51 +58,52 @@ void setup() {
   Serial.begin(9600);
   WHL_setup();
   LCD_setup();
-  IR_setup();
+//  IR_setup();
 }
 
 void loop() {
-  if(IrReceiver.decode()){
-        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
-            Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
-            // We have an unknown protocol here, print extended info
-            IrReceiver.printIRResultRawFormatted(&Serial, true);
-            IrReceiver.resume(); // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
-        } else {
-            IrReceiver.resume(); // Early enable receiving of the next IR frame
-            IrReceiver.printIRResultShort(&Serial);
-            IrReceiver.printIRSendUsage(&Serial);
-        }
-        Serial.println();
-
-        /*
-         * Finally, check the received data and perform actions according to the received command
-         */
-        if (IrReceiver.decodedIRData.command == 0x10) {
-            // do something
-        } else if (IrReceiver.decodedIRData.command == 0x11) {
-            // do something else
-        }
+  while(Serial.available())
+  { 
+    cmd = Serial.read();
+    switch(cmd)
+    {
+      case 'w': w.forward(); break;
+      case 'x': w.back(); break;
+      case 'a': w.forwardLeft(); break;
+      case 'd': w.forwardRight(); break;
+      case 'z': w.backLeft(); break;
+      case 'c': w.backRight(); break;
+      case 's': w.stop(); break;
+      case '1': w.setSpeedLeft(75); break;
+      case '2': w.setSpeedLeft(200); break;
+      case '9': w.setSpeedRight(75); break;
+      case '0': w.setSpeedRight(200); break;
+      case '5': w.setSpeed(100); break;
+    }
   }
-//  while(Serial.available())
-//  { 
-//    cmd = Serial.read();
-//    switch(cmd)
-//    {
-//      case 'w': w.forward(); break;
-//      case 'x': w.back(); break;
-//      case 'a': w.forwardLeft(); break;
-//      case 'd': w.forwardRight(); break;
-//      case 'z': w.backLeft(); break;
-//      case 'c': w.backRight(); break;
-//      case 's': w.stop(); break;
-//      case '1': w.setSpeedLeft(75); break;
-//      case '2': w.setSpeedLeft(200); break;
-//      case '9': w.setSpeedRight(75); break;
-//      case '0': w.setSpeedRight(200); break;
-//      case '5': w.setSpeed(100); break;
-//    }
-//  }
+//  if(IrReceiver.decode()){
+//        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+//            Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
+//            // We have an unknown protocol here, print extended info
+//            IrReceiver.printIRResultRawFormatted(&Serial, true);
+//            IrReceiver.resume(); // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
+//        } else {
+//            IrReceiver.resume(); // Early enable receiving of the next IR frame
+//            IrReceiver.printIRResultShort(&Serial);
+//            IrReceiver.printIRSendUsage(&Serial);
+//        }
+//        Serial.println();
+//
+//        /*
+//         * Finally, check the received data and perform actions according to the received command
+//         */
+//        if (IrReceiver.decodedIRData.command == 0x10) {
+//            // do something
+//        } else if (IrReceiver.decodedIRData.command == 0x11) {
+//            // do something else
+//        }
+}
+
 //  uint8_t barPos = 0;
 //  argIn = Serial.parseInt(SKIP_ALL);
 //  if(argIn <= 0) return;
@@ -120,4 +121,3 @@ void loop() {
 //    lcd.write(0);
 //    delay(20);
 //  }
-}
