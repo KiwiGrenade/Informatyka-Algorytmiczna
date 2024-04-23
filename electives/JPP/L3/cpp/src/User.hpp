968e7f97872a578dafac328ec42875f74901b0f0
Jakub Jaskow <jakub.jaskow@nokia.com>
2024-04-23 22:57:12 +0200

    Add User class definition. TODO: Add tests.

--- electives/JPP/L3/cpp/src/User.hpp
+++ electives/JPP/L3/cpp/src/User.hpp
@@ -1,1 +1,38 @@
+#include <random>
+#include <stdexcept>
 #include "DHSetup.hpp"
+
+template<typename T>
+class User {
+private:
+    std::mt19937 rng;
+    std::uniform_int_distribution<uint32_t> dist;
+    DHSetup DH;
+    uint32_t secret;
+    T key;
+
+public:
+    User(DHSetup& _DH) {
+        DH = _DH;
+        dist = 
+            std::uniform_int_distribution<uint32_t> 
+            (0, std::numeric_limits<uint32_t>::max());
+        secret = dist(rng);
+    }
+    
+    T getPublicKey() {
+        DH.power(DH.getGenerator(), secret);
+    }
+
+    void setKey(T a) {
+        DH.power(a, secret);
+    }
+
+    T encrypt(T m) {
+        return m * key;
+    }
+
+    T decrypt(T c) {
+        return c / key;
+    }
+};
