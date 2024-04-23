4223b640f98ab9ae1010cdd06fcf12072917cbf3
Jakub Jaskow <jakub.jaskow@nokia.com>
2024-04-23 19:21:19 +0200

    Add DHSetup and tests for it

---
+++ electives/JPP/L3/cpp/libs/GF.hpp
@@ -0,0 +1,55 @@
+//
+// Created by jaskow on 06.04.24.
+//
+
+#ifndef JPP_L2_GF_HPP
+#define JPP_L2_GF_HPP
+
+#include <cstdint>
+#include <iostream>
+
+class GF {
+private:
+    uint32_t p;
+    uint32_t val;
+    static void checkP(const GF& L, const GF& R);
+    static uint32_t inverse(const uint32_t _p, const int64_t _val);
+
+public:
+    explicit GF(const uint32_t& _p, const int64_t& _val);
+    ~GF() = default;
+
+    [[nodiscard]] uint32_t getP() const noexcept;
+    void setP(uint32_t _p) noexcept;
+    [[nodiscard]] uint32_t getVal() const noexcept;
+    void setVal(int64_t _val) noexcept;
+
+    // assignments
+    GF& operator= (const GF &other) = default;
+    GF& operator+=(const GF &other);
+    GF& operator-=(const GF &other);
+    GF& operator*=(const GF &other);
+    GF& operator/=(const GF &other);
+
+    // compare
+    bool operator == (const GF &R) const;
+    bool operator != (const GF &R) const;
+    bool operator <  (const GF &R) const;
+    bool operator >  (const GF &R) const;
+    bool operator <= (const GF &R) const;
+    bool operator >= (const GF &R) const;
+
+    // arithmetic
+    GF operator-() const noexcept;
+    GF operator+() const noexcept;
+    GF operator+(const GF &R) const;
+    GF operator-(const GF &R) const;
+    GF operator*(const GF &R) const;
+    GF operator/(const GF &R) const;
+
+    // stream
+    friend std::ostream& operator<<(std::ostream& out, const GF& gf);
+
+};
+
+#endif //JPP_L2_GF_HPP
---
+++ electives/JPP/L3/cpp/test.cpp
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <random>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_adapters.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
+#include "DHSetup.hpp"
+
+TEST_CASE("1234567891"){  
+    // constexpr uint32_t testRuns = 100;
+    constexpr uint32_t p = 1234567891;
+    DHSetup<uint32_t> DH = DHSetup<uint32_t>(p);
+    // constexpr uint32_t min_val = 0; 
+    // constexpr uint32_t max_val = 100;
+    constexpr uint32_t n_tests = 10;
+    constexpr uint32_t min_val = 0; 
+    SECTION("power()") { 
+        constexpr uint32_t base_max_val = 5;
+        constexpr uint32_t pow_max_val = 10;
+        uint32_t a = GENERATE(take(n_tests, random(min_val, base_max_val)));
+        uint32_t b = GENERATE(take(n_tests, random(min_val, pow_max_val)));
+        CHECK(DH.power(a, b) == pow((long double)a,b));
+    }
+    // SECTION("getGenerator()") {
+    //     
+    // }
+}
