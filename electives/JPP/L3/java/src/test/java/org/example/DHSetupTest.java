9051e0d390f54fe2f41daea2f9acbca120a7f548
Mango T14 <kubajaskow@wp.pl>
2024-05-02 21:13:00 +0200

    Add test for DHSetup and include JUnit in pom file

---
+++ electives/JPP/L3/java/src/test/java/org/example/DHSetupTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DHSetupTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void power() {
+    }
+}
