017064d36646a0d53b267240bcabe8174d934d36
Mango T14 <kubajaskow@wp.pl>
2024-05-09 11:25:44 +0200

    Refactor ex3

--- electives/PWL/L5/ex3.pl
+++ electives/PWL/L5/ex3.pl
@@ -1,31 +1,35 @@
-traverse(i, [H | T], Stack) :- 
-    H =.. [_ | [Son | _]],
-    append([Son], [H | T], Stack), !.
+in(i).
+out(o).
+next(n).
+prev(p).
 
-traverse(n, [H, Parent | T], Stack) :- 	
-    Parent =.. X,
-    append(_, [H, Brother | _], X),
-    append([Brother], [Parent | T], Stack), !.
+browse(Drzewo):-
+    browse(Drzewo,[],[]).
 
-traverse(p, [H, Parent | T], Stack) :- 	
-    Parent =.. X,
-    append(_, [Brother, H | _], X),
-    append([Brother], [Parent | T], Stack), !.
+browse(Korzen,Lewo,Prawo):-
+    writeln(Korzen),
+    write('command: '),
+    read(Polecenie),
+    (   in(Polecenie) -> (   Korzen =..[_,Korzen1 | Lewo1] ->
+                             (   browse(Korzen1,Lewo1, []))
+                         ; true
+                         ) ; true
+    ),
+    (   next(Polecenie) ->
+           (Lewo = [H2|R2] ->
+                        (   I2 is 1,
+                            append([Korzen],Prawo,Prawo2),
+                            browse(H2, R2, Prawo2)
+                        ) ; I2 is 0
+           ) ; I2 is 0
+    ),
+    (   prev(Polecenie) ->
+           (Prawo = [H3|R3] ->
+                        (   I3 is 1,
+                            append([Korzen], Lewo, Lewo3),
+                            browse(H3, Lewo3, R3)
+                        ) ;   I3 is 0
+           ) ; I3 is I2
+    ),
+    (   (out(Polecenie); I3 = 1)-> true; browse(Korzen,Lewo,Prawo)).
 
-traverse(o, [ _ ], _).
-
-traverse(o, [_, Parent | T], Stack) :- 	
-    append([Parent], T, Stack), !.
-
-traverse(_, X, X).
-
-readAction([]).
-readAction([H | T]) :- 	
-    writeq(H),
-    write('\ncommand: '),
-    read(Command),
-    traverse(Command, [H | T], Stack),
-    readAction(Stack).
-
-browse(Term) :- 
-    readAction([Term]).
